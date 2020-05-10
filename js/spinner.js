fa35f36db505d43c42725ccde60f9fc5a319b370
Prahlad Makwana <prahlad@jetbro.in>
2020-05-10 14:52:53 +0530

    update spinner all

--- js/spinner.js
+++ js/spinner.js
@@ -83,8 +83,10 @@ class Spinner
                 bounds: el })[
             0];
 
-            /* stepper.addEventListener("dragend", this.onEndDragging(stepper,stepperEl,prev,next,container));
-            stepper.addEventListener("drag", this.clickAndHold(stepper,stepperEl,prev,next,container)); */
+            stepper.addEventListener("dragend", function(){
+                this.onEndDragging(stepper,stepperEl,prev,next,container)}.bind(this));
+            stepper.addEventListener("drag", function(){
+                 this.clickAndHold(stepper,stepperEl,prev,next,container)}.bind(this)); 
                     
             //On click and hold on the Stepper
             $(stepperEl).on("touchstart mousedown", function () {
@@ -102,6 +104,46 @@ class Spinner
             console.log($(el).find('.number_spinner_next')); 
             next.addEventListener("click", function(){this.next(stepper,stepperEl,container); }.bind(this)); 
             prev.addEventListener("click", function(){this.prev(stepper,stepperEl,container); }.bind(this)); 
+
+            //Cursor Events --- only on desktop
+
+            $(stepperEl).mousemove(function (e) {
+                var target = e.target;
+                var cursor = $(target).parents('.number_spinner_bound').parent().find('.cursor'); 
+                const rect = el.getBoundingClientRect(); 
+                var left = rect.left + window.scrollX;
+                var top = rect.top + window.scrollY; 
+                var x = e.pageX - left;
+                var y = e.pageY - top; 
+                x = x;
+                y = y - 70;
+                $(cursor).css({ left: x, top: y });
+               // $(".cursor").css({ left: e.pageX - 40, top: e.pageY - 40 });
+            });
+            
+            $(stepperEl).mousedown(function (e) {
+                var target = e.target;
+                var cursor = $(target).parents('.number_spinner_bound').parent().find('.cursor');  
+                TweenMax.to(cursor, 0.2, { opacity: 1, scale: 0.8, ease: Power2.easeOut });
+            });
+            
+            $(document).mouseup(function (e) {
+                var target = e.target;
+                var cursor = $(target).parents('.number_spinner_bound').parent().find('.cursor'); 
+                var clicked = new TimelineMax();
+                clicked.to('.cursor', 0.5, { scale: 3, ease: Power2.easeOut }, "+=0"),
+                clicked.to('.cursor', 0.2, { opacity: 0, ease: Power2.easeOut }, "-=0.2"),
+                clicked.to('.cursor', 0, { scale: 1, ease: Power2.easeOut }, "-=0");
+            });
+
+            //On click and hold on the Stepper
+            $(stepperEl).on("touchstart mousedown", function () {
+                this.moveOnHold(stepper,stepperEl,prev,next,container);
+            }.bind(this));
+
+            $(stepperEl).on("touchend mouseup", function () {
+                this.resetTimer();
+            }.bind(this));
 
          }.bind(this)); 
     }
@@ -134,21 +176,22 @@ class Spinner
         setTimeout(function () {stepperEl.classList.remove("disabled");}, 100);
       };
     onEndDragging(stepper,stepperEl,prev,next,container) { 
-        TweenMax.to(stepper, 1, { x: "0", ease: Elastic.easeOut.config(1, 0.75) }); // brings it back in position
+        TweenMax.to(stepperEl, 1, { x: "0", ease: Elastic.easeOut.config(1, 0.75) }); // brings it back in position
         this.changeValueOnDraggingRelease(stepper,stepperEl,prev,next,container);
         this.resetTimer();  
         stepperEl.classList.add("disabled");
     }
     changeValueOnDraggingRelease(stepper,stepperEl,prev,next,container){ 
         this.anim.progress(1, false);
-        if (stepper.hitTest(next, this.overlapThreshold) && this.myval < this.maxValue) {
+        var myval = container.dataset.qty;
+        if (stepper.hitTest(next, this.overlapThreshold) && myval < this.maxValue) {
             this.increaseValue(container); 
-        } else if (stepper.hitTest(prev, this.overlapThreshold) && this.myval > this.minValue) {
+        } else if (stepper.hitTest(prev, this.overlapThreshold) && myval > this.minValue) {
             this.decreaseValue(container); 
         }
         this.anim.resume();
     
-    }
+    } 
     //Reset Timer
     resetTimer() {
         clearTimeout(this.mainTimer);
@@ -158,66 +201,45 @@ class Spinner
         this.checkanddisable(stepper,stepperEl,prev,next,container);
     }
     checkanddisable(stepper,stepperEl,prev,next,container){
-        if (stepper.hitTest(next, this.overlapThreshold) && this.myval >= this.maxValue) {
+        var myval = container.dataset.qty;
+        if (stepper.hitTest(next, this.overlapThreshold) && myval >= this.maxValue) {
             stepperEl.classList.add("disabled");
         } else
-        if (stepper.hitTest(prev, this.overlapThreshold) && this.myval <= this.minValue) {
+        if (stepper.hitTest(prev, this.overlapThreshold) && myval <= this.minValue) {
             stepperEl.classList.add("disabled");
         }
     }
-    moveOnHold = function (stepper,stepperEl,prev,next,container) {
-        mainTimer = setTimeout(function () {
+    moveOnHold(stepper,stepperEl,prev,next,container) {
+        this.mainTimer = setTimeout(function () {
           this.ChangeNumberOnHold(stepper,stepperEl,prev,next,container);
-          this.moveOnHold();
-        }, NewTimer);
+          this.moveOnHold(stepper,stepperEl,prev,next,container);
+        }.bind(this), this.NewTimer);
     }
     ChangeNumberOnHold(stepper,stepperEl,prev,next,container) {
-        if (stepper.hitTest(next, this.overlapThreshold) && this.myval < this.maxValue) {
+        var myval = container.dataset.qty;
+        if (stepper.hitTest(next, this.overlapThreshold) && myval < this.maxValue) {
             stepperEl.classList.remove("disabled");
             this.increaseValue(container); 
-          this.decreaseTimer();
+            this.decreaseTimer();
         } else
-      
-        if (stepper.hitTest(prev, this.overlapThreshold) && this.myval > this.minValue) {
+        
+        if (stepper.hitTest(prev, this.overlapThreshold) && myval > this.minValue) {
             stepperEl.classList.remove("disabled");
-          this.decreaseValue(container);
-          this.decreaseTimer();
+            this.decreaseValue(container);
+            this.decreaseTimer();
         } else
         {
-          NewTimer = InitialTimer;
+            NewTimer = this.InitialTimer;
         }
-      }
-}  
- 
-
-
-var decreaseTimer = function () {
-    if (NewTimer > minTimerValue) {
-        NewTimer = NewTimer / increment;
-        console.log("timer: " + NewTimer);
-    } else
-    {
-        NewTimer = minTimerValue;
-        console.log("timer: " + NewTimer);
     }
-};
- 
-  
- 
-  //Cursor Events --- only on desktop
-  
-  $("#stepper").mousemove(function (e) {
-    $(".cursor").css({ left: e.pageX - 40, top: e.pageY - 40 });
-  });
-  
-  $("#stepper").mousedown(function (e) {
-    TweenMax.to(".cursor", 0.2, { opacity: 1, scale: 0.8, ease: Power2.easeOut });
-  });
-  
-  $(document).mouseup(function (e) {
-    var clicked = new TimelineMax();
-    clicked.to(".cursor", 0.5, { scale: 3, ease: Power2.easeOut }, "+=0"),
-    clicked.to(".cursor", 0.2, { opacity: 0, ease: Power2.easeOut }, "-=0.2"),
-    clicked.to(".cursor", 0, { scale: 1, ease: Power2.easeOut }, "-=0");
-  });
-  
+    decreaseTimer = function () {
+        if (this.NewTimer > this.minTimerValue) {
+            this.NewTimer = this.NewTimer / this.increment;
+            console.log("timer: " + this.NewTimer);
+        } else
+        {
+            this.NewTimer = this.minTimerValue;
+            console.log("timer: " + this.NewTimer);
+        }
+    }
+}  
